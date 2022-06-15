c928ee2a51e9f5b76f2702f9b013acbf19a58e4c
Dennis Johnson <densarjohn@gmail.com>
2022-06-15 18:44:47 +0530

    Added 6.99 as weights as defaults for each conv channel

--- decore.py
+++ decore.py
@@ -1,24 +1,30 @@
-import torch.nn.utils.prune as prune
-import torch
+import torch, torch.nn.utils.prune as prune
 
 class DecorePruningStrategy(prune.BasePruningMethod):
     '''
     Prune every other entry in a tensor
     '''
-    PRUNING_TYPE = 'unstructured'
-
-    def compute_mask(self, t, default_mask):
-        assert(default_mask.dim() == 4), f"Expected 4 dims (OIFF), got {default_mask.dim()}"
+    def compute_mask(self, t_unused, default_mask):
+        '''
+        Overriden method, applies a channel mask to the layer.
+        '''
+        channel_mask =  self.decore_channel_mask(default_mask)
         mask = default_mask.clone()
-
-        for out_dim in range(default_mask.shape[0]):
-            if out_dim % 2 != 0:
-                mask[out_dim] = 0
-                assert (torch.numel(mask[out_dim]) - torch.count_nonzero(mask[out_dim])) == torch.numel(mask[out_dim])
-            else:
-                assert torch.count_nonzero(mask[out_dim]) == torch.numel(mask[out_dim])
-
+        for channel_num, chn_mask in enumerate(channel_mask):
+            mask[channel_num] *= chn_mask
         return mask
+        
+    def decore_channel_mask(self, default_mask):
+        '''
+        Compute the channel mask given some weights. 
+        1. A weight Wj is given for each channel in the layer.
+        2. Init each channel weight=6.99 so prob of keeping ~= 1.
+        3. Use these probs to draw either 0, 1 from a Bernoulli dist. 
+        '''
+        weights = torch.zeros(default_mask.shape[0]) + 6.99
+        probs   = torch.sigmoid(weights)
+        return torch.bernoulli(probs)
+
 
 def decore_structured(module, name):
     '''
